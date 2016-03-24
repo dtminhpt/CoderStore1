362bd3502ac2954ae261f921be60c70f55f599c1
Minh Dinh <dtminhpt@gmail.com>
2016-03-24 20:58:31 +0700

    milestone5

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe Product, type: :model do
     it "should sort products by name alphabetically" do
       @products = Product.alphabetical
       expect(@products).to eq [@product2, @product3, @product1]
-     end
+    end
+  end
+
+  describe "discount" do 
+    before do
+      @product1 = Product.create! price_vnd: 99000
+      @product2 = Product.create! price_vnd: 120000
+      @product3 = Product.create! price_vnd: 202000
+      @product4 = Product.create! price_vnd: 900000
+    end
+
+    it "should discount 21% when hat cost > 100000 VND" do
+      expect(@product1.delivered_price_vnd).to eq (@product1.price_vnd * (1 - 0.21))
+    end
   end
 end
